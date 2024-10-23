cc0ab27189792d08ff0ba14cfbfcf69d99a7cd98
duduh96 <eduardo.rocha@rascal.com.br>
2024-10-23 11:23:22 -0300

    primeiroup

---
+++ numeros 1.py
@@ -0,0 +1,16 @@
+n1 = 3
+n2 = 4
+
+soma = n1 + n2
+
+print("O resultado da soma é", soma, "Obrigado por perguntar")
+
+n1 = int(input("Entre com um numero: "))
+n2 = input("Entre com outro numero: ")
+#print(type(n2))
+n2 = int(n2)
+#print(type(n2))
+soma = n1 + n2
+
+print("O resultado da soma é", soma, "Obrigado por perguntar")
+print("alteracao de arquivo")
